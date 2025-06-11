832529abcb10b0d68a86f9619e124728d421faae
keywi <keywi@UNKNOWN_LINUX>
2025-06-10 19:14:34 -0500

    Agregar archivos de proyecto.

---
+++ Bodega_Kiarita/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bodega_Kiarita.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
